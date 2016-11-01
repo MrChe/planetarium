76dd15b7104e9659dade81b08257e637ea3f855e
LeonidKuznetsov <appleboyleonid@gmail.com>
2016-11-01 09:50:17 +0200

    feat(project):add content and styles

--- app/blocks/news-slider/news-slider.js
+++ app/blocks/news-slider/news-slider.js
@@ -1,15 +1,15 @@
 import 'swiper';
 
 var newsSlider = new Swiper('.news-slider__slider-container', {
-	slidesPerView: 2,
+	slidesPerView: 3,
 	spaceBetween: 45,
-	centeredSlides: true,
+	loop: true,
 	nextButton: '.swiper-button-next',
 	prevButton: '.swiper-button-prev',
 	breakpoints: {
 	// when window width is <= 767px
 		767: {
-			slidesPerView: 1,
+			slidesPerView: 1
 			// spaceBetween: 10
 		}
 	}
--- app/blocks/promo-slider/promo-slider.js
+++ app/blocks/promo-slider/promo-slider.js
@@ -2,5 +2,6 @@ import 'swiper';
 
 var promoSlider = new Swiper('.promo-slider__slider-container', {
 	nextButton: '.swiper-button-next',
-	prevButton: '.swiper-button-prev'
+	prevButton: '.swiper-button-prev',
+	loop: true
 });
